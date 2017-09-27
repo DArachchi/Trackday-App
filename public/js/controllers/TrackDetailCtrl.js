b463c4171da32f7961fe5126a0baf717b93d431c
Dino Ferrari <DArachchi@gmail.com>
2017-09-26 23:49:39 -0500

    Added sort by track length and number of turns

--- public/js/controllers/TrackDetailCtrl.js
+++ public/js/controllers/TrackDetailCtrl.js
@@ -4,6 +4,5 @@ angular.module('track-detail', ['ngRoute'])
     .controller('TrackDetailCtrl', ['$scope', '$routeParams', 'Track',
         function($scope, $routeParams, Track) {
             $scope.track = Track.get({trackId: $routeParams.trackId}, function(track) {
-                console.log($scope.track);
             })
     }]);
